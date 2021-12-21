5b155f8ffec4fbe0aca793516507a953f055d329
Tanja <tanja120a@gmail.com>
2021-12-21 13:27:33 +0100

    Finished Header component Auth flow

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,13 +1,30 @@
 import React from 'react';
-
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 class Header extends React.Component {
+    renderContent() {
+        switch(this.props.auth){
+            case null:
+                return ;
+            case false:
+                return ( <li><a href="/auth/google">Login with Google</a></li>        );
+            default:
+                return (<li> <a href="/api/logout">Logout</a></li>)
+        }
+    }
+
     render() {
         return(
             <nav>
                 <div className="nav-wrapper">
-                <a href="/" className="left brand-logo">Emaily</a>
+                <Link 
+                    to={this.props.auth ? '/surveys': "/"} 
+                    className="left brand-logo"
+                >
+                    Emaily
+                </Link>
                 <ul  className="right ">
-                    <li><a href="/auth/google">Login with Google</a></li>                    
+                { this.renderContent()}               
                 </ul>
                 </div>
             </nav>
@@ -16,4 +33,8 @@ class Header extends React.Component {
     
 };
 
-export default Header;
+function mapStateToProps({auth}) {
+    return { auth }
+}
+
+export default connect(mapStateToProps)(Header);
--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,7 +1,7 @@
  import { FETCH_USER } from '../actions/types';
 
  const authReducer = (state = null, action) => {
-    console.log(action);
+   // console.log(action);
     switch(action.type) {
         case FETCH_USER:
             return action.payload || false;
