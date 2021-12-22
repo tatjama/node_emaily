5406787677b72263a187d7833127254bc721cfed
Tanja <tanja120a@gmail.com>
2021-12-22 06:55:08 +0100

    Billing with Stripe finished

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import Payments from './Payments';
 class Header extends React.Component {
     renderContent() {
         switch(this.props.auth){
@@ -9,7 +10,11 @@ class Header extends React.Component {
             case false:
                 return ( <li><a href="/auth/google">Login with Google</a></li>        );
             default:
-                return (<li> <a href="/api/logout">Logout</a></li>)
+                return [
+                    <li key = "1"><Payments/></li>,
+                    <li key = "3" style = {{ margin: " 0 10px" }}> Credits: { this.props.auth.credits } </li>,
+                    <li key = "2"> <a href="/api/logout">Logout</a></li>
+                ]
         }
     }
 
