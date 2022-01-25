2fedf2c2b09b08e66b1a678b0356ac23fd6ae23b
Tanja <tanja120a@gmail.com>
2022-01-25 09:28:09 +0100

    SurveyFormReview
    Create SurveyFormreview,
    Submit Form
    Redirect to Dashboard,
    Empty Form with Cancel

--- client/src/components/surveys/SurveyNew.js
+++ client/src/components/surveys/SurveyNew.js
@@ -1,15 +1,34 @@
 import React, { Component} from 'react';
+import { reduxForm } from 'redux-form';
 import SurveyForm from './SurveyForm';
+import SurveyFormReview from './SurveyFormReview';
 
 class SurveyNew extends Component{
+    state = { showFormReview: false }
+
+    renderContent() {
+        if (this.state.showFormReview){
+            return(
+                <SurveyFormReview
+                    onCancel = {() => this.setState({ showFormReview: false})}
+                />
+            ) 
+        }
+
+        return(
+            <SurveyForm onSurveySubmit = {() => this.setState({showFormReview: true})}/>
+        ) 
+    }
+
     render(){
         return(
             <div>
-                Survey New
-                <SurveyForm/>
+                { this.renderContent() }
             </div>
         )
     }
 }
 
-export default SurveyNew;
+export default reduxForm({
+    form: 'surveyForm'
+}) (SurveyNew);
